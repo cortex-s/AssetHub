7c55bce6b6fcc839710ff6f14114b99c7744333e
Supercruise <249873864+cortex-s@users.noreply.github.com>
2026-03-19 01:18:12 +0700

    nearly completed for /api/assets

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
-import authRoutes from "./routes/auths.js";
-import meRoutes from "./routes/me.js";
-import categoryRoutes from "./routes/categories.js";
-import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
+// Middleware
+import { errorMiddleware } from "./middlewares/error.js";
 import { authMiddleware } from "./middlewares/auth.js";
 
 const port = process.env.PORT;
@@ -16,10 +14,20 @@ app.use(cookieParser());
 app.get("/", (req, res) => {
   return res.send("Hello, World");
 });
+
+// Routes
+import authRoutes from "./routes/auths.js";
+import meRoutes from "./routes/me.js";
+import categoryRoutes from "./routes/categories.js";
+import assetRoutes from "./routes/assets.js";
+
+// Manage Routes
 app.use("/api/auth", authRoutes);
 app.use(authMiddleware);
 app.use("/api/@me", meRoutes);
 app.use("/api/categories", categoryRoutes);
+app.use("/api/assets", assetRoutes);
+// app.use("/api/borrows")
 app.use(errorMiddleware);
 
 app.listen(port, () => console.log("Running at port:", port));
