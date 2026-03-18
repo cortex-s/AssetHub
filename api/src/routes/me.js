fc8a5296e79b8544e4b32f4e3511a4b303c0de73
Supercruise <249873864+cortex-s@users.noreply.github.com>
2026-03-19 01:25:40 +0700

    this is collection of `user / viewer account routes`

--- api/src/routes/me.js
+++ api/src/routes/me.js
@@ -4,5 +4,5 @@ import { meControllers } from "../controllers/me/index.js";
 const router = Router();
 
 router.get("/", meControllers.info);
-
+// borrows - show anything that I borrowed
 export default router;
